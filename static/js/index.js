864ad983f02bb60681d92592b66adac05aacbea9
Sanchit <sanchitcop19@gmail.com>
2026-07-14 12:43:26 -0700

    add project links

--- static/js/index.js
+++ static/js/index.js
@@ -9,3 +9,19 @@ var switch_devpost = function(toggle) {
       blue.style = "display: none";
     }
   };
+
+var toggleProjects = function() {
+    var modal = document.getElementById("projects-modal");
+    var toggleBtn = document.getElementById("projects-toggle");
+    var isOpen = modal.classList.toggle("open");
+    toggleBtn.setAttribute("aria-expanded", isOpen ? "true" : "false");
+  };
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") {
+      var modal = document.getElementById("projects-modal");
+      if (modal && modal.classList.contains("open")) {
+        toggleProjects();
+      }
+    }
+  });
